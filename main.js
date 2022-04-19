9218e02c09131e1340161df5d6c7ec56a0f7bfa8
Taek Been Nam <taekbeen93@gmail.com>
2022-04-19 19:17:30 -0400

    scroll to page

--- main.js
+++ main.js
@@ -13,3 +13,13 @@ document.addEventListener('scroll', () => {
     }
 });
 
+// Scroll to selected page when the user selects a page on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link === undefined) return;
+    
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: "smooth", block: "start"});
+})
